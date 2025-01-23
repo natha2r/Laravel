9d18d1db0d6bb9360698292ce1a0cc141c7d1f47
Nathalia Galvis <karolgalvis10@gmail.com>
2025-01-23 00:29:30 -0500

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\ProveedorController;
+
+Route::get('empleados', [EmpleadoController::class, 'index']); 
+Route::post('empleados', [EmpleadoController::class, 'store']);
+Route::get('empleados/{id}', [EmpleadoController::class, 'show']);
+Route::put('empleados/{id}', [EmpleadoController::class, 'update']);
+Route::delete('empleados/{id}', [EmpleadoController::class, 'destroy']);
+
+Route::post('proveedores', [ProveedorController::class, 'store']);
+Route::get('proveedores', [ProveedorController::class, 'index']);
+Route::get('proveedores/{id}', [ProveedorController::class, 'show']);
+Route::put('proveedores/{id}', [ProveedorController::class, 'update']);
+Route::delete('proveedores/{id}', [ProveedorController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
